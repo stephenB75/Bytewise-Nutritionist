590a7888dd9adb16b305955135e1bcfa36ec34d6
StephenB <stephen75@me.com>
2025-08-02 00:09:52 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+// ByteWise PWA Service Worker
+const CACHE_NAME = 'bytewise-v1';
+const urlsToCache = [
+  '/Bytewise-Nutritionist/',
+  '/Bytewise-Nutritionist/index.html',
+  '/Bytewise-Nutritionist/manifest.json',
+  '/Bytewise-Nutritionist/offline.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request).catch(() => {
+          // If both cache and network fail, show offline page
+          if (event.request.destination === 'document') {
+            return caches.match('/Bytewise-Nutritionist/offline.html');
+          }
+        });
+      })
+  );
+});
