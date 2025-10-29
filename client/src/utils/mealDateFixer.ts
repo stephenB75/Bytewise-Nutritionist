4a95dc1623e51ba4c4815fe83cc1d3134177769c
StephenB <stephen75@me.com>
2025-10-29 13:35:00 -0400

    Delete client directory

--- client/src/utils/mealDateFixer.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Meal Date Fixer Utility
- * Fixes meal date mismatches
- */
-
-export function fixMealDateMismatches(meals: any[]) {
-  // Simple implementation - just return the meals as is
-  return meals;
-}
